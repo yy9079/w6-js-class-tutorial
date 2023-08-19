6816b915cde754f564775678450ad7a8f19f3e99
Sato Yui <shangangyouyi@gmail.com>
2023-08-20 01:16:22 +0900

    クラスとインスタンス課題作成

--- accordion.js
+++ accordion.js
@@ -1,24 +1,51 @@
 (() => {
-  const $elm = document.querySelector("#js-accordion");
-  const $trigger = $elm.getElementsByTagName("a");
+  // クラスとインスタンス
+  class Accordion {
+    // 初期化
+    constructor(obj) {
+      console.log("obj", obj);
+      const $elm = document.querySelector(obj.hookName);
+      const $trigger = $elm.getElementsByTagName(obj.tagName);
 
-  const $triggerLen = $trigger.length;
-  let index = 0;
-  while (index < $triggerLen) {
-    $trigger[index].addEventListener("click", (e) => clickHandler(e));
-    index++;
+      const $triggerLen = $trigger.length;
+      let index = 0;
+      while (index < $triggerLen) {
+        // クラスの中で関数を指定した場合は、clickHandler(e)の前に「this」を入れなければならない
+        $trigger[index].addEventListener("click", (e) => this.clickHandler(e));
+        index++;
+      }
+    }
+
+    // クリックしたら実行される処理
+    // クラスの中で関数を書くときは、const不要なので消す
+    clickHandler = (e) => {
+      e.preventDefault();
+
+      const $target = e.currentTarget;
+
+      const $content = $target.nextElementSibling;
+      if ($content.style.display === "block") {
+        $content.style.display = "none";
+      } else {
+        $content.style.display = "block";
+      }
+    };
   }
 
-  const clickHandler = (e) => {
-    e.preventDefault();
+  // インスタンスを生成 生成するときは変数を必ず指定する
+  //ここでid、タグを変えられる→constructorで呼び出す
+  const fuckingAccordion = new Accordion({
+    hookName: "#js-faq",
+    tagName: "p",
+  });
 
-    const $target = e.currentTarget;
+  const dummyAccordion = new Accordion({
+    hookName: "#js-accordion",
+    tagName: "a",
+  });
 
-    const $content = $target.nextElementSibling;
-    if ($content.style.display === "block") {
-      $content.style.display = "none";
-    } else {
-      $content.style.display = "block";
-    }
-  };
+  const miniAccordion = new Accordion({
+    hookName: "#js-accordion-mini",
+    tagName: "dt",
+  });
 })();
